34cb95f2a588b7a88c4693467308c4c62288a2e0
Robert Neal <robertwneal2@gmail.com>
2022-02-11 09:45:32 -0800

    setup class framework

--- questions.rb
+++ questions.rb
@@ -5,6 +5,77 @@ class QuestionsDatabase < SQLite3::Database
     include Singleton
 
     def initialize
+        super('questions.db')
+        self.type_translation = true
+        self.results_as_hash = true
+    end
+end
+
+class User
+
+    attr_accessor
+
+    def self.find_by_id
+
+    end
+
+    def initialize(options)
+
+    end
+
+end
+
+class Question
+
+    attr_accessor
+
+    def self.find_by_id
+
+    end
+
+    def initialize(options)
+
+    end
+
+end
+
+class QuestionFollow
+
+    attr_accessor
+
+    def self.find_by_id
+
+    end
+
+    def initialize(options)
+
+    end
+
+end
+
+class Reply
+
+    attr_accessor
+
+    def self.find_by_id
+
+    end
+
+    def initialize(options)
+
+    end
+
+end
+
+class QuestionLike
+
+    attr_accessor
+
+    def self.find_by_id
+
+    end
+
+    def initialize(options)
 
     end
 
